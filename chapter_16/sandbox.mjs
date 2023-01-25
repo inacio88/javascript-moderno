364e8d3b49160394affafc0f6a169b716d24b0fd
inacio88 <inacio88@outlook.com>
2023-01-24 21:23:49 -0300

    final do todo list

--- chapter_16/sandbox.mjs
+++ chapter_16/sandbox.mjs
@@ -1,31 +1,63 @@
 import {db, app} from "./firebase.mjs"
-import { collection, addDoc, setDoc, getDoc, getDocs, Timestamp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js";
+import { collection, addDoc,doc, setDoc, where,getDoc,query, deleteDoc, getDocs, Timestamp, onSnapshot } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js";
 
-console.log(db)
-console.log(app)
+// console.log(db)
+// console.log(app)
 const list = document.querySelector('ul');
 const receitas = collection(db, "recipes");
 const form = document.querySelector('form');
-const addRecipe = (recipe) => {
+const button = document.querySelector('button');
+const addRecipe = (recipe, id) => {
     let html = `
-    <li>
+    <li data-id='${id}'>
         <div> ${recipe.title} </div>
         <div> ${recipe.created_at.toDate()} </div>
-
+        <button class='btn btn-danger btn-sm my-2'>delete</button>
     </li>
     `
     list.innerHTML += html;
-    console.log(html);
+    // console.log(html);
+}
+
+const deleteRecipe = (id) =>{
+    const recipes = document.querySelectorAll('li');
+    recipes.forEach(recipe => {
+        if (recipe.getAttribute('data-id') === id) {
+            recipe.remove();
+        }
+    })
 }
 
 const querySnapshot = await getDocs(receitas);
-querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-//   console.log(doc.id, " => ", doc.data());
-    addRecipe(doc.data())
+// querySnapshot.forEach((doc) => {
+//   // doc.data() is never undefined for query doc snapshots
+// //   console.log(doc.id, " => ", doc.data());
+//     addRecipe(doc.data(), doc.id)
 
+// });
+
+const q = query(collection(db, "recipes"));
+const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    querySnapshot.docChanges().forEach((change)=>{
+        console.log(change)
+        const doc = change.doc
+        if(change.type === 'added'){
+            addRecipe(doc.data(), doc.id)
+        }
+        else if(change.type === 'removed'){
+            deleteRecipe(doc.id)
+        }
+
+    })
+
+    querySnapshot.forEach((doc) => {
+        console.log(doc.data().title)
+    });
 });
 
+button.addEventListener('click', () => {
+    unsubscribe();
+})
 // db.collection('recipes').get().then(snapshot => {
 //     // console.log(snapshot);
 //     snapshot.docs.forEach(doc => {
@@ -48,3 +80,14 @@ form.addEventListener('submit', e => {
     const docRef =  addDoc(collection(db, "recipes"), recipe);
     
 })
+
+//delete
+
+list.addEventListener('click', e =>{
+    console.log(e)
+    if(e.target.tagName === 'BUTTON'){
+        const id = e.target.parentElement.getAttribute('data-id');
+        // console.log(id);
+        deleteDoc(doc(db, "recipes", id));
+    }
+});
