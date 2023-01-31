76edb0f2ae4076c01db24dda601cce36e1ea26f7
inacio88 <inacio88@outlook.com>
2023-01-31 16:00:59 -0300

    fim do curso

---
+++ project_ui_library/webpack.config.js
@@ -0,0 +1,56 @@
+// opções que queremo como o webpack funcione
+// é necessário node, isso não vai rodar no navegador
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/assets'),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    // devServer: {
+    //     contentBase: path.resolve(__dirname, 'dist'),
+    //     directory: path.join(__dirname, "assets"),
+        // publicPath: '/assets/',
+        // compress: true,
+        // port: 8080,
+        // hot: true
+    // }
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+
+        },
+        devMiddleware: {
+            index: true,
+            mimeTypes: { "text/html": ["phtml"] },
+            publicPath: "/assets/",
+            serverSideRender: true,
+            writeToDisk: true,
+        },
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }
+    
+    
+    
+    
+    ]
+    }
+};
